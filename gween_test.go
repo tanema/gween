e6614f4dcaf8351092655e5a4cf4eceae11c8f2c
Nicholas Rich <nick.o.rich@gmail.com>
2022-12-07 23:38:12 -0700

    test: Add test for gween updating in reverse with negative

--- gween_test.go
+++ gween_test.go
@@ -115,6 +115,15 @@ func TestTween_UpdateNeg(t *testing.T) {
 	assert.False(t, isFinished)
 }
 
+func TestTween_UpdateNegReverse(t *testing.T) {
+	tween := New(0, 10, 10, ease.Linear)
+	tween.Update(2)
+	tween.reverse = true
+	current, isFinished := tween.Update(-1)
+	assert.Equal(t, float32(3), current)
+	assert.False(t, isFinished)
+}
+
 func TestTween_Defaults_Forward(t *testing.T) {
 	tween := New(0, 10, 10, ease.Linear)
 	assert.False(t, tween.reverse)
